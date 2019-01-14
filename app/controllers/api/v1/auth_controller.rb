4fe93da6c6f61b4992d8bde260ba0f6dcac919b4
no-relation <eddiec76@gmail.com>
2019-01-14 17:11:27 -0600

    authorization working

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AuthController < Api::V1::ApplicationController
     def create 
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-            render user, methods: [ :token ]
+            render json: user, methods:[ :token ]
         else
             render json: {
                 error: 'username or password are incorrect', 
