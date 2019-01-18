c65f248815778de685ff20a183291371b78a6ef5
no-relation <eddiec76@gmail.com>
2019-01-18 15:41:39 -0600

    sending back feelings with days

--- app/controllers/api/v1/feelings_controller.rb
+++ app/controllers/api/v1/feelings_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::FeelingsController < Api::V1::ApplicationController
     def create
         day = Day.find_or_create_by(fulldate: params[:fulldate])
         feeling = Feeling.create(rating: params[:rating], user_id: params[:user_id], day_id: day.id)
-        byebug
         render json: feeling.user.feelings
     end
 
@@ -24,6 +23,13 @@ class Api::V1::FeelingsController < Api::V1::ApplicationController
     def destroy
         current_feeling.destroy
         render json: current_feeling
+    end
+
+    def userfind
+        user = User.find(params[:id])
+        feelings = user.feelings
+        days = user.feelings.map{ |feeling| feeling.day }
+        render json: feelings.zip(days)
     end
 
     def define_current_feeling
