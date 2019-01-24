40919561d4e89fc7268b736f0c6b689282c6c5d5
no-relation <eddiec76@gmail.com>
2019-01-24 11:04:21 -0600

    build static front end again again

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35901734adca0299ad7c00c8e994da06.js"
+  "/precache-manifest.ab8656539cc855f4028fa350244607d5.js"
 );
 
 workbox.clientsClaim();
