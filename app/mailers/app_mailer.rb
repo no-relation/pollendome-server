9050400d21b14e19184b1973d06d9b6b2377e3fe
no-relation <eddiec76@gmail.com>
2019-04-26 16:22:51 -0500

    mailer working

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,10 +3,14 @@ class AppMailer < ApplicationMailer
     layout 'mailer'
 
     def new_day_log(day)
+        dayParamsList = day.keys.map do |key|
+            "#{key}: #{day[key]}"
+        end
+        dayParams = dayParamsList.join("\n")
         mail(
             to: "eddiec76@gmail.com", 
-            subject: "POLLENDOME created day #{day.id} - #{day.fulldate}", 
-            body: 
+            subject: "POLLENDOME created day #{day[:id]} - #{day[:fulldate]}", 
+            body: dayParams
         )
     end
 
